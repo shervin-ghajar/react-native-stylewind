cb08e32db98051b382b5e6039ea063814cac0f6b
Shervin Ghajar <ssghajar.work@gmail.com>
2024-12-30 19:27:01 +0330

    feat: logs added

--- dist/utils/createTheme.js
+++ dist/utils/createTheme.js
@@ -1,6 +1,8 @@
 import { PROJECT_ROOT_PATH } from '../configs/constatns';
 import { defaultTheme } from '../configs/defaultTheme';
+import debug from 'debug';
 import merge from 'lodash/merge';
+const log = debug('react-native-tailwind');
 /* -------------------------------------------------------------------------- */
 export const createTheme = () => {
     let theme = {};
@@ -8,10 +10,12 @@ export const createTheme = () => {
         // Try to import theme.config.ts
         // eslint-disable-next-line @typescript-eslint/no-require-imports
         theme = require(PROJECT_ROOT_PATH).default; // Adjust the path based on your app structure
+        log('createTheme');
+        log('Current theme configuration:', { PROJECT_ROOT_PATH, theme });
     }
     catch (_a) {
         console.warn('No theme.config.ts found, using default theme configs.');
     }
-    console.log("createTheme", { theme });
+    console.log('createTheme', { theme });
     return merge(defaultTheme, theme);
 };
--- src/utils/createTheme.ts
+++ src/utils/createTheme.ts
@@ -1,19 +1,23 @@
 import { PROJECT_ROOT_PATH } from '../configs/constatns';
 import { defaultTheme } from '../configs/defaultTheme';
 import { Theme } from '../types';
+import debug from 'debug';
 import merge from 'lodash/merge';
 
+const log = debug('react-native-tailwind');
 /* -------------------------------------------------------------------------- */
 export const createTheme = (): Theme => {
-  let theme={}
+  let theme = {};
   try {
     // Try to import theme.config.ts
 
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    theme = require(PROJECT_ROOT_PATH).default;  // Adjust the path based on your app structure
+    theme = require(PROJECT_ROOT_PATH).default; // Adjust the path based on your app structure
+    log('createTheme');
+    log('Current theme configuration:', { PROJECT_ROOT_PATH, theme });
   } catch {
     console.warn('No theme.config.ts found, using default theme configs.');
   }
-  console.log("createTheme",{theme})
+  console.log('createTheme', { theme });
   return merge(defaultTheme, theme);
 };
