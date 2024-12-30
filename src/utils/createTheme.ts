7f92409b88fc918b4221d14ed93e0e4eee4b5161
Shervin Ghajar <ssghajar.work@gmail.com>
2024-12-30 19:48:38 +0330

    feat: logs added

--- src/utils/createTheme.ts
+++ src/utils/createTheme.ts
@@ -2,7 +2,7 @@ import { PROJECT_ROOT_PATH } from '../configs/constatns';
 import { defaultTheme } from '../configs/defaultTheme';
 import { Theme } from '../types';
 import merge from 'lodash/merge';
-import path from 'path';
+import path, { resolve } from 'path';
 
 /* -------------------------------------------------------------------------- */
 export const createTheme = (): Theme => {
@@ -10,12 +10,7 @@ export const createTheme = (): Theme => {
   try {
     // Try to import theme.config.ts
 
-    console.log(
-      'Current theme configuration:1',
-      { theme, dir: __dirname, path: path.dirname('') },
-      __dirname,
-      __filename,
-    );
+    console.log('Current theme configuration:1', resolve(PROJECT_ROOT_PATH));
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     theme = require(PROJECT_ROOT_PATH).default; // Adjust the path based on your app structure
     console.log('Current theme configuration:1', { PROJECT_ROOT_PATH, theme });
