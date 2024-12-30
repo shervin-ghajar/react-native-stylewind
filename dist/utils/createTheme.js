32ee635c34561d6355ee12e1b6f8ae207e17e67d
Shervin Ghajar <ssghajar.work@gmail.com>
2024-12-30 19:38:03 +0330

    feat: logs added

--- dist/utils/createTheme.js
+++ dist/utils/createTheme.js
@@ -1,17 +1,15 @@
 import { PROJECT_ROOT_PATH } from '../configs/constatns';
 import { defaultTheme } from '../configs/defaultTheme';
-import debug from 'debug';
 import merge from 'lodash/merge';
-const log = debug('react-native-tailwind');
 /* -------------------------------------------------------------------------- */
 export const createTheme = () => {
     let theme = {};
     try {
         // Try to import theme.config.ts
+        console.log('Current theme configuration:1', { theme });
         // eslint-disable-next-line @typescript-eslint/no-require-imports
         theme = require(PROJECT_ROOT_PATH).default; // Adjust the path based on your app structure
-        log('createTheme');
-        log('Current theme configuration:', { PROJECT_ROOT_PATH, theme });
+        console.log('Current theme configuration:1', { PROJECT_ROOT_PATH, theme });
     }
     catch (_a) {
         console.warn('No theme.config.ts found, using default theme configs.');
