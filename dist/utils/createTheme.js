fbb5dfd67342d098c4b90d4b0ea179efbfbeaa85
Shervin Ghajar <ssghajar.work@gmail.com>
2024-12-30 19:45:12 +0330

    feat: logs added

--- dist/utils/createTheme.js
+++ dist/utils/createTheme.js
@@ -1,12 +1,13 @@
 import { PROJECT_ROOT_PATH } from '../configs/constatns';
 import { defaultTheme } from '../configs/defaultTheme';
 import merge from 'lodash/merge';
+import path from 'path';
 /* -------------------------------------------------------------------------- */
 export const createTheme = () => {
     let theme = {};
     try {
         // Try to import theme.config.ts
-        console.log('Current theme configuration:1', { theme });
+        console.log('Current theme configuration:1', { theme, dir: __dirname, path: path.dirname('') }, __dirname, __filename);
         // eslint-disable-next-line @typescript-eslint/no-require-imports
         theme = require(PROJECT_ROOT_PATH).default; // Adjust the path based on your app structure
         console.log('Current theme configuration:1', { PROJECT_ROOT_PATH, theme });
--- src/utils/createTheme.ts
+++ src/utils/createTheme.ts
@@ -2,6 +2,7 @@ import { PROJECT_ROOT_PATH } from '../configs/constatns';
 import { defaultTheme } from '../configs/defaultTheme';
 import { Theme } from '../types';
 import merge from 'lodash/merge';
+import path from 'path';
 
 /* -------------------------------------------------------------------------- */
 export const createTheme = (): Theme => {
@@ -9,7 +10,12 @@ export const createTheme = (): Theme => {
   try {
     // Try to import theme.config.ts
 
-    console.log('Current theme configuration:1', { theme }, __dirname, __filename);
+    console.log(
+      'Current theme configuration:1',
+      { theme, dir: __dirname, path: path.dirname('') },
+      __dirname,
+      __filename,
+    );
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     theme = require(PROJECT_ROOT_PATH).default; // Adjust the path based on your app structure
     console.log('Current theme configuration:1', { PROJECT_ROOT_PATH, theme });
