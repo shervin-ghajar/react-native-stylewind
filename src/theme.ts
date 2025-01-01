ac65f64a98a1a016ebf1d0ee2c19d2acf3abf8f2
Shervin Ghajar <ssghajar.work@gmail.com>
2025-01-01 13:23:46 +0330

    build: process refactored

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,6 @@ import { Theme } from './types';
 import { createTheme } from './utils/createTheme';
 import { themeConfigWatcher } from './utils/themeWatcher';
 
-export const theme: Theme =  createTheme();
+export const theme: Theme = createTheme();
 
 if (process.env.NODE_ENV !== 'production') themeConfigWatcher();
--- src/utils/createTheme.ts
+++ src/utils/createTheme.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+require('ts-node/register');
 import chalk from 'chalk';
 import { CONSUMER_ROOT_PATH, THEME_CONFIG_FILE } from '../configs/constatns/index';
 import { defaultTheme } from '../configs/defaultTheme';
@@ -5,22 +7,23 @@ import { Theme } from '../types';
 import _ from 'lodash';
 import path from 'path';
 import fs from 'fs';
-
 /* -------------------------------------------------------------------------- */
-export const createTheme =  (): Theme => {
+export const createTheme = (): Theme => {
   let theme = {};
   try {
     // Try to import theme.config.ts
-    const themConfigPath=path.resolve(CONSUMER_ROOT_PATH, THEME_CONFIG_FILE)
-    console.log("resolve",themConfigPath,{existsSync:fs.existsSync(themConfigPath)})
-    console.log("PATH",themConfigPath,CONSUMER_ROOT_PATH,THEME_CONFIG_FILE)
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const themeConfigFile = require(themConfigPath); // Adjust the path based on your app structure
-    console.log({themeConfigFile})
-    theme=themeConfigFile.default
-  } catch {
+    const themeConfigPath = path.resolve(CONSUMER_ROOT_PATH, THEME_CONFIG_FILE);
+    console.log("resolve", themeConfigPath, { existsSync: fs.existsSync(themeConfigPath) });
+    console.log("PATH", themeConfigPath, CONSUMER_ROOT_PATH, THEME_CONFIG_FILE);
+
+    // Use dynamic import
+    const themeConfigFile = require(themeConfigPath);
+    console.log({ themeConfigFile });
+    theme = themeConfigFile.default; // Access the default export
+  } catch (error) {
     console.warn('No theme.config.ts found, using default theme configs.');
     console.log(chalk.yellow(`WARN: No theme.config.ts found, using default theme configs.`));
+    console.error(error);
   }
   console.log('createTheme', { theme });
   return _.merge(defaultTheme, theme);
