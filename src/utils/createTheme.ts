b5f12e0880cdfe4256367821499190f2bc8c8b83
Shervin Ghajar <ssghajar.work@gmail.com>
2025-01-01 12:49:41 +0330

    build: process refactored

--- src/utils/createTheme.ts
+++ src/utils/createTheme.ts
@@ -4,16 +4,18 @@ import { defaultTheme } from '../configs/defaultTheme';
 import { Theme } from '../types';
 import _ from 'lodash';
 import path from 'path';
+import fs from 'fs';
 
 /* -------------------------------------------------------------------------- */
 export const createTheme = (): Theme => {
   let theme = {};
   try {
     // Try to import theme.config.ts
-    console.log("resolve",path.resolve(path.dirname('')))
-    console.log("PATH",path.resolve(CONSUMER_ROOT_PATH, THEME_CONFIG_FILE),CONSUMER_ROOT_PATH,THEME_CONFIG_FILE)
+    const themConfigPath=path.resolve(CONSUMER_ROOT_PATH, THEME_CONFIG_FILE)
+    console.log("resolve",themConfigPath,{existsSync:fs.existsSync(themConfigPath)})
+    console.log("PATH",themConfigPath,CONSUMER_ROOT_PATH,THEME_CONFIG_FILE)
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    theme = require(path.resolve(CONSUMER_ROOT_PATH, THEME_CONFIG_FILE)).default; // Adjust the path based on your app structure
+    theme = require(themConfigPath).default; // Adjust the path based on your app structure
   } catch {
     console.warn('No theme.config.ts found, using default theme configs.');
     console.log(chalk.yellow(`WARN: No theme.config.ts found, using default theme configs.`));
