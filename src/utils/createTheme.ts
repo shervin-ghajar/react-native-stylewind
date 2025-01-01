17416d627b40b11d6c9a517a4754608c9c415f67
Shervin Ghajar <ssghajar.work@gmail.com>
2025-01-01 11:23:07 +0330

    build: process refactored

--- src/utils/createTheme.ts
+++ src/utils/createTheme.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { CONSUMER_ROOT_PATH, THEME_CONFIG_FILE } from '../configs/constatns/index';
 import { defaultTheme } from '../configs/defaultTheme';
 import { Theme } from '../types';
@@ -9,11 +10,13 @@ export const createTheme = (): Theme => {
   let theme = {};
   try {
     // Try to import theme.config.ts
+    console.log("resolve",path.resolve(path.dirname('')))
     console.log("PATH",path.resolve(CONSUMER_ROOT_PATH, THEME_CONFIG_FILE),CONSUMER_ROOT_PATH,THEME_CONFIG_FILE)
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     theme = require(path.resolve(CONSUMER_ROOT_PATH, THEME_CONFIG_FILE)).default; // Adjust the path based on your app structure
   } catch {
     console.warn('No theme.config.ts found, using default theme configs.');
+    console.log(chalk.yellow(`WARN: No theme.config.ts found, using default theme configs.`));
   }
   console.log('createTheme', { theme });
   return _.merge(defaultTheme, theme);
