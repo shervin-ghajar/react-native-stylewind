b98ad8c4b9e72f544b07871e520abedcb00aa106
ssghajar <ssghajar.work@gmail.com>
2025-01-03 19:42:46 +0330

    fix: build config

--- dist/rebuild.js
+++ dist/rebuild.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import { execSync } from 'child_process';
 import require$$0 from 'path';
+import { fileURLToPath } from 'url';
 
 try {
     console.log('Regenerating theme...');
-    const configPath = require$$0.resolve(__dirname, '../../rollup.config.mjs');
+    const configPath = require$$0.resolve(fileURLToPath(import.meta.url), '../../rollup.config.mjs');
     // Run the build script defined in package.json
     console.log('Regenerating theme...');
     execSync(`npx rollup -c ${configPath}`, { stdio: 'inherit' });
