72fd3ad1349de97ed933008cf82d28002f7c779d
Shervin Ghajar <ssghajar.work@gmail.com>
2025-02-18 18:52:28 +0330

    feat: withRNTailwind.js

--- src/utils/withRNTailwind.ts
+++ src/utils/withRNTailwind.ts
@@ -1,8 +1,23 @@
 import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
 export default function withRNTailwind(metroConfigs: any) {
   // Watch for changes in the specific file
   console.log('withRNTailwind');
   execSync('npx init-rn-tailwind');
+  const fileToWatch = path.resolve(process.cwd(), 'theme.config.js'); // Change to your file path
+  console.log('fileToWatch', fileToWatch);
+  fs.watch(fileToWatch, (eventType) => {
+    if (eventType === 'change') {
+      console.log(`File ${fileToWatch} has been changed. Running command...`);
+      try {
+        execSync('npx generate-rn-Tailwind', { stdio: 'inherit' });
+        console.log('Command executed successfully.');
+      } catch (error) {
+        console.error('Error executing command:', error);
+      }
+    }
+  });
   return metroConfigs;
 }
