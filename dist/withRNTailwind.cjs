080cd9058d7741e0e380ee8e00785925abbef699
Shervin Ghajar <ssghajar.work@gmail.com>
2025-02-18 19:05:46 +0330

    feat: withRNTailwind.js

--- dist/withRNTailwind.cjs
+++ dist/withRNTailwind.cjs
@@ -8,7 +8,7 @@ function withRNTailwind(metroConfigs) {
     // Watch for changes in the specific file
     console.log('withRNTailwind');
     child_process.execSync('npx init-rn-tailwind');
-    const fileToWatch = path.resolve(process.cwd(), 'theme.config.js'); // Change to your file path
+    const fileToWatch = path.resolve(process.cwd(), 'theme.config.cjs'); // Change to your file path
     console.log('fileToWatch', fileToWatch);
     fs.watch(fileToWatch, (eventType) => {
         if (eventType === 'change') {
