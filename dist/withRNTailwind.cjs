8e4e7b9f71ad060f490acc6cfa2db64287b16010
Shervin Ghajar <ssghajar.work@gmail.com>
2025-02-18 18:54:50 +0330

    feat: withRNTailwind.js

--- dist/withRNTailwind.cjs
+++ dist/withRNTailwind.cjs
@@ -1,11 +1,27 @@
 'use strict';
 
 var child_process = require('child_process');
+var fs = require('fs');
+var path = require('path');
 
 function withRNTailwind(metroConfigs) {
     // Watch for changes in the specific file
     console.log('withRNTailwind');
     child_process.execSync('npx init-rn-tailwind');
+    const fileToWatch = path.resolve(process.cwd(), 'theme.config.js'); // Change to your file path
+    console.log('fileToWatch', fileToWatch);
+    fs.watch(fileToWatch, (eventType) => {
+        if (eventType === 'change') {
+            console.log(`File ${fileToWatch} has been changed. Running command...`);
+            try {
+                child_process.execSync('npx generate-rn-Tailwind', { stdio: 'inherit' });
+                console.log('Command executed successfully.');
+            }
+            catch (error) {
+                console.error('Error executing command:', error);
+            }
+        }
+    });
     return metroConfigs;
 }
 
